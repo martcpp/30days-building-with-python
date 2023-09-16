3a3e96460da5f4cd4a000ed2a78bf04178b70b94
martcpp <mordecaiemmanueletukudo@gmail.com.com>
2023-09-16 17:20:48 +0100

    day1

---
+++ day1/basic.py
@@ -0,0 +1,35 @@
+import math # import math module
+
+# interger
+print(3+4) # addition
+print(3-4) # substraction
+print(3*4) # multiplication
+print(3/4) # division
+print(3**5) # 3^5 or power 
+
+#strings
+
+print("Hello World") # double quotes string
+print('Hello World' + ' ' + 'Hello World') # concatenation
+print(r'Hello World \"quote\" ') # raw string with escape character
+print(f'Hello World'*3) # string multiplication
+
+# variables
+name = 'John Doe' # string variable
+number = 3 # integer variable
+float_number = 3.14 # float variable
+
+# math module
+float_number2 = floor(float_number) # floor function
+float_number3 = ceil(float_number) # ceil function
+float_number4 = sqrt(float_number) # sqrt function
+float_number5 = pow(float_number, 2) # pow function
+float_number6 = pi # pi constant
+
+# string methods
+name2 = name.upper() # upper method
+name3 = name.lower() # lower method
+name4 = name.capitalize() # capitalize method
+name5 = name.title() # title method
+print('title:', name5)
+
